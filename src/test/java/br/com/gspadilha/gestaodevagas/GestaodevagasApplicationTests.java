3f21d1e5e509a99a2cab07d8a43c9bcedf7dc85f
Guilherme Padilha <gs.padilha@gmail.com>
2024-01-06 14:18:35 -0300

    feat: inicialização do projeto com o Lombok

---
+++ src/test/java/br/com/gspadilha/gestaodevagas/GestaodevagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gspadilha.gestaodevagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodevagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
