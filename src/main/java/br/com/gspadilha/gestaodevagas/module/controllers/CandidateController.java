59015c23c7dc0cf45f4274c0fc64e4f24f4489f1
Guilherme Padilha <gs.padilha@gmail.com>
2024-01-06 15:26:27 -0300

    feat: criando repositório

--- src/main/java/br/com/gspadilha/gestaodevagas/module/controllers/CandidateController.java
+++ src/main/java/br/com/gspadilha/gestaodevagas/module/controllers/CandidateController.java
@@ -4,8 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gspadilha.gestaodevagas.module.entities.CandidateEntity;
+import br.com.gspadilha.gestaodevagas.module.repositories.CandidateRepository;
 import jakarta.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,7 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
     }
 }
