9a9ed15e84696436ae97f4bf2944f9279866c043
Guilherme Padilha <gs.padilha@gmail.com>
2024-01-06 15:10:53 -0300

    feat: criando tabela de candidatos

--- src/main/java/br/com/gspadilha/gestaodevagas/module/entities/CandidateEntity.java
+++ src/main/java/br/com/gspadilha/gestaodevagas/module/entities/CandidateEntity.java
@@ -1,15 +1,25 @@
 package br.com.gspadilha.gestaodevagas.module.entities;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidates")
 public class CandidateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaços!")
@@ -20,4 +30,9 @@ public class CandidateEntity {
     private String email;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
