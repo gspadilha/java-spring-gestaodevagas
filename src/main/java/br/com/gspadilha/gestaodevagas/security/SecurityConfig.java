6253bca7b19cb6e32fbf0ab311cce166db8d9741
Guilherme Padilha <gs.padilha@gmail.com>
2024-01-16 05:35:47 -0300

    feat: configurando rotas públicas

--- src/main/java/br/com/gspadilha/gestaodevagas/security/SecurityConfig.java
+++ src/main/java/br/com/gspadilha/gestaodevagas/security/SecurityConfig.java
@@ -1,5 +1,9 @@
 package br.com.gspadilha.gestaodevagas.security;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +12,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    List<String> routesPermitted = new ArrayList<>(Arrays.asList("/candidate/", "/company/"));
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    for (String route : routesPermitted) {
+                        auth.requestMatchers(route).permitAll();
+                    }
+                    auth.anyRequest().authenticated();
+                });
         return http.build();
     }
 }
