7aa7446f878252bad028a09408589ea22e09510e
Guilherme Padilha <gs.padilha@gmail.com>
2024-01-27 09:39:44 -0300

    feat: hash da senha do candidato

--- src/main/java/br/com/gspadilha/gestaodevagas/module/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/gspadilha/gestaodevagas/module/useCases/CreateCandidateUseCase.java
@@ -1,6 +1,7 @@
 package br.com.gspadilha.gestaodevagas.module.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.gspadilha.gestaodevagas.exceptions.UserFoundException;
@@ -13,6 +14,9 @@ public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         var username = candidateEntity.getUsername();
         var email = candidateEntity.getEmail();
@@ -22,6 +26,9 @@ public class CreateCandidateUseCase {
                 .ifPresent(user -> {
                     throw new UserFoundException();
                 });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
